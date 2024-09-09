951ff40a6b655fa30c4c07ef787f057dae2cd484
kim <goldfish.jinyu@gmail.com>
2024-09-09 21:57:55 +0800

    fix(error ): runtime error

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 poetry run celery -A app.celery worker -P eventlet --loglevel=info &
-poetry run gunicorn -w 8 -k gthread -b 0.0.0.0:5002 app:app
+poetry run gunicorn -w 8 -k eventlet --timeout 120 -b 0.0.0.0:5002 app:app
